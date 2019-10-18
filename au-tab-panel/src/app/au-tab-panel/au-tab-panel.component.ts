1128464a2f832e845cff4573f14fc6b65a148b11
Erick Morales Silva <erick.silva@localhost.localdomain>
2019-10-18 16:16:49 -0500

    v0.1.4-au-tab-panel.3 - au-tab-panel, initial functionality.

--- au-tab-panel/src/app/au-tab-panel/au-tab-panel.component.ts
+++ au-tab-panel/src/app/au-tab-panel/au-tab-panel.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ContentChildren, AfterContentInit, QueryList } from '@angular/core';
+import { AuTabComponent } from 'app/au-tab/au-tab.component';
 
 @Component({
   selector: 'au-tab-panel',
   templateUrl: './au-tab-panel.component.html',
   styleUrls: ['./au-tab-panel.component.scss']
 })
-export class AuTabPanelComponent implements OnInit {
+export class AuTabPanelComponent implements OnInit, AfterContentInit {
+
+  @ContentChildren(AuTabComponent)
+  tabs: QueryList<AuTabComponent>;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  ngAfterContentInit() {
+    const selectedTab = this.tabs.find(tab => tab.selected)
+
+    if (!selectedTab) {
+      this.tabs.first.selected = true;
+    }
+  }
+
+  selectTab(tab: AuTabComponent) {
+    this.tabs.forEach(tab => tab.selected = false);
+    tab.selected = true;
+  }
 }
