c028bc9be33cb89e6fed08691bf270c7aed77a2d
erick.silva <ericksilva@localhost.localdomain>
2019-10-19 16:42:30 -0500

    v0.1.4-au-tab-panel.4 - Custom header panel template / TemplateRef / Template partials.

--- au-tab-panel/src/app/au-tab-panel/au-tab-panel.component.ts
+++ au-tab-panel/src/app/au-tab-panel/au-tab-panel.component.ts
@@ -1,4 +1,15 @@
-import { Component, OnInit, ContentChildren, AfterContentInit, QueryList } from '@angular/core';
+import { 
+  Component, 
+  OnInit, 
+  ContentChildren, 
+  AfterContentInit, 
+  QueryList, 
+  TemplateRef, 
+  AfterViewInit,
+  Input
+
+} from '@angular/core';
+
 import { AuTabComponent } from 'app/au-tab/au-tab.component';
 
 @Component({
@@ -6,20 +17,28 @@ import { AuTabComponent } from 'app/au-tab/au-tab.component';
   templateUrl: './au-tab-panel.component.html',
   styleUrls: ['./au-tab-panel.component.scss']
 })
-export class AuTabPanelComponent implements OnInit, AfterContentInit {
+export class AuTabPanelComponent implements OnInit, AfterContentInit, AfterViewInit {
 
   @ContentChildren(AuTabComponent)
   tabs: QueryList<AuTabComponent>;
 
+  @Input('header-template') 
+  headerTemplate: TemplateRef<any>
+
   constructor() { }
 
   ngOnInit() {
+    
+  }
+
+  ngAfterViewInit() {
+    
   }
 
   ngAfterContentInit() {
     const selectedTab = this.tabs.find(tab => tab.selected)
 
-    if (!selectedTab) {
+    if (!selectedTab && this.tabs.first) {
       this.tabs.first.selected = true;
     }
   }
@@ -27,5 +46,11 @@ export class AuTabPanelComponent implements OnInit, AfterContentInit {
   selectTab(tab: AuTabComponent) {
     this.tabs.forEach(tab => tab.selected = false);
     tab.selected = true;
+  }
+
+  get tabsContext() {
+    return {
+      tabsInstance: this.tabs
+    };
   }
 }
